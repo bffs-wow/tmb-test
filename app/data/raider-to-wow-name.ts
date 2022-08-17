89a4383457d1ae5821dc210c52f8cccb4b888409
Sean Arthur <seanmarthur@gmail.com>
2022-08-17 15:19:08 -0400

    Auto-generated commit [ci skip]

---
+++ app/data/raider-to-wow-name.ts
@@ -0,0 +1,9 @@
+/**
+ * If the raider's name in the loot list does not exactly match their in game name (special chars, etc.),
+ * certain functions may not work. Use this mapping to map raider names to their actual in game name.
+ */
+
+ export const raiderToWowNameMap = {
+   'Dritzz':'Drîtzz',
+   'Id': 'Ïd'
+ }
